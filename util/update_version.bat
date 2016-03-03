702a69c6effd84961043b142d3c13e2488ec86f5
Aean <v@aean.net>
2016-03-04 03:26:53 +0800

    fixed the auto version batch. i simply don't need the hash, dear linus. just give me the tag and commit counts please. i would hardly make a branch.

--- util/update_version.bat
+++ util/update_version.bat
@@ -1,5 +1,8 @@
-echo #define GIT_VER     \> VersionNo.h
-git describe --abbrev=0 --always --tags --dirty=-mod>> VersionNo.h
+:: ugly batch + abuse of git commit count, but works great for me.
+
+<NUL set /p=#define GIT_VER > VersionNo.h
+for /f %%i in ('git describe --long --always --tags') do set TAGNAME=%%i
+echo %TAGNAME:~0,-9% >> VersionNo.h
 echo #define STR2(v) #v >> VersionNo.h
 echo #define STR(v) STR2(v) >> VersionNo.h
 echo #define STRFILEVER STR(GIT_VER) >> VersionNo.h 
